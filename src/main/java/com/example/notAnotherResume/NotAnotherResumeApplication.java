40ec9db9202761149b89a21dd0c4d0f95374b60a
Venne143 <vennelavollala19@gmail.com>
2024-12-04 17:02:09 +0530

    Initial commit

---
+++ src/main/java/com/example/notAnotherResume/NotAnotherResumeApplication.java
@@ -0,0 +1,13 @@
+package com.example.notAnotherResume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotAnotherResumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotAnotherResumeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/notAnotherResume/NotAnotherResumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notAnotherResume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotAnotherResumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
